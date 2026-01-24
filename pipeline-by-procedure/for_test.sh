5e11a1307b1f2b4b19809fb6415859e4f9ecee2d
Overtake <1753497822@qq.com>
2026-01-24 12:18:44 +0800

    Update for_test.sh

--- pipeline-by-procedure/for_test.sh
+++ pipeline-by-procedure/for_test.sh
@@ -1,6 +1,6 @@
 #数据预处理，生成3D结构
-python 1_generate_pdbqt_with_charge.py
+python 1_generate_pdbqt_with_charge.py 
 #分配任务，用于并行 python 2_distribution_dock_task.py <task_num> vina_cpu默认占用1，可自行调整
-python 2_distribution_dock_task.py 1
+python 2_distribution_dock_task.py 4
 #提取对接数据
 python  3_from_vina_2record.py
